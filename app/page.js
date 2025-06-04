01e9da0ad69d7105ef5c2aed021832e18212f9b8
Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-06-04 14:06:43 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
+    <div className="py-32">
       Hi
       <br/>
       <Button>Click me</Button>
